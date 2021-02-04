dba7e140ad06cb08940a7c178e1b9c453615dd57
JakobBundgaard <56150649+JakobBundgaard@users.noreply.github.com>
2021-02-04 11:25:17 +0100

    2.exercise

--- 2.exercises/intro_ex.js
+++ 2.exercises/intro_ex.js
@@ -31,4 +31,68 @@ console.log(currentYear);
 
 console.log(+year + number);
 
-;// --------------------------------------
+// --------------------------------------
+
+// Exercise 3 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+const num3 = Number(numberOne) + Number(numberTwo);
+
+console.log(num3);
+
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+const num4 = Number(anotherNumberOne) + Number(anotherNumberTwo);
+
+console.log(num4.toFixed(2));
+console.log(typeof (num4));
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+
+const num5 = (10 + 45 + 98) / 3;
+
+console.log(num5.toFixed(5));
+// Show in the console the avg. with 5 decimals
+
+
+
+
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+const letters = "abc";
+// Get me the character "c"
+
+console.log(letters[2]);
+
+
+
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+const fact = "You are learning javascript!";
+
+// capitalize the J in Javascript
+
+console.log(fact.replace("j", "J"));
+
+// --------------------------------------
