603d5902e463fc39025fb101b9ce8c59e8aa4593
JakobBundgaard <jakobbundgaard@hotmail.com>
2021-02-11 10:24:11 +0100

    Update functions.js

--- 0.misc/1._introEx/functions.js
+++ 0.misc/1._introEx/functions.js
@@ -2,8 +2,23 @@ function myFirstFunction() {
     return "Hello";
 }
 
-const myVariableFunction = function () {
-
+const myVariableFunction = () => {
+    console.log("Hello")
 };
+
+// callback functions
+
+
+const sayHi = () => {
+    return console.log("Hi");
+}
+
+function sayHilater2(anyFunctionReference) {
+    anyFunctionReference();
+}
+
+sayHiLater(sayHi());
+
+//hoisting 
 
 
