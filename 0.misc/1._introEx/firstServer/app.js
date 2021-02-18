77e4c4459a2166983bd8c7f5b48143c10c566ac6
JakobBundgaard <jakobbundgaard@hotmail.com>
2021-02-18 10:20:03 +0100

    all

--- 0.misc/1._introEx/firstServer/app.js
+++ 0.misc/1._introEx/firstServer/app.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
 // app.get("/", (req, res) => {
 //     res.send({ message: "This is my response" })
@@ -15,37 +15,39 @@ const app = express();
 // To solve the assigment create a datatype to store the data in 
 // Implement the two endpoints.. 
 
-const student = [
-    {
-        name: "Jake",
-        id: 2
-    },
-    {
-        name: "Shawn",
-        id: 3
-    }
-];
+// const students = [
+//     {
+//         name: "Jake",
+//         id: 2
+//     },
+//     {
+//         name: "Shawn",
+//         id: 3
+//     }
+// ];
 
-app.get("/students", (req, res) => {
-    res.send({ "Students": student })
-});
+// app.get("/students", (req, res) => {
+//     res.send({ "Students": student })
+// });
 
-const classes = [
-    {
-        name: "Node.js",
-        teacher: "Anders Latif"
-    },
-    {
-        name: "Python",
-        teacher: "Claus Boven"
-    }
-];
+// const classes = [
+//     {
+//         name: "Node.js",
+//         teacher: "Anders Latif"
+//     },
+//     {
+//         name: "Python",
+//         teacher: "Claus Boven"
+//     }
+// ];
 
-app.get("/students/classes", (req, res) => {
-    res.send({ "Classes": classes })
-})
+// app.get("/students/classes", (req, res) => {
+//     res.send({ "Classes": classes })
+// })
 
-app.listen(8080);
+// app.listen(8080);
+
+
 
 
 
---
+++ 0.misc/1._introEx/variables.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let variableA;
+let variableB;
+
+// console.log(variableA, variableB); // Don't ever do this!!!
+
+// console.log(variableA, variableB);
+
+
+
+// {
+//     let scopedVariable = "abc";
+//     {
+//         scopedVariable = 123;
+//     }
+//     console.log(scopedVariable);
+// }
+
+// {
+//     let scopedVariable = "abc";
+//     {
+//         let scopedVariable = 123;
+//         console.log(scopedVariable);
+//     }
+//     console.log(scopedVariable);
+// }
+
+for (let i = 0; i < 5; i++) {
+    setTimeout(() => {
+        console.log(i);
+    }, 1000);
+}
+
+
