ef7665bda1097a1be7b4d853836e63818b3ea94a
JakobBundgaard <jakobbundgaard@hotmail.com>
2021-02-18 11:21:59 +0100

    a

--- 0.misc/1._introEx/firstServer/server1.js
+++ 0.misc/1._introEx/firstServer/server1.js
@@ -1,6 +1,22 @@
 const express = require("express");
-const app = express();
+const app = express(); // app is theinitialized instanceof server
 // console.log(express());
+
+app.get("/", (req, res) => {
+    res.send({});
+})
+
+app.get("/welcome", (req, res) => {
+    res.send("<h2>Welcome!</h2>");
+});
+
+app.get("/me", (req, res) => {
+    res.send(jakob = {
+        firstName: "Jakob",
+        lastName: "Bundgaard",
+        needs: "Food"
+    });
+})
 
 
 
