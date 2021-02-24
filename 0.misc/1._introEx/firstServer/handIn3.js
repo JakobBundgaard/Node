07d68872e91cf41ec85e0fc0bb9d9bb855cbeb20
JakobBundgaard <jakobbundgaard@hotmail.com>
2021-02-24 12:09:01 +0100

    almost done

--- 0.misc/1._introEx/firstServer/handIn3.js
+++ 0.misc/1._introEx/firstServer/handIn3.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
+
+
+let dateObj = new Date();
+
+
+console.log(dateObj)
+
 app.get("/time", (req, res) => {
-    app.send("Hello")
+    res.send("The time is: " + dateObj.toLocaleTimeString());
+})
+
+app.get("/day", (req, res) => {
+    res.send("The day is: " + dateObj.toDateString().slice(0, 3));
+})
+
+app.get("/month", (req, res) => {
+    res.send("The month is: " + dateObj.toDateString().slice(3, 7));
 })
 
 app.listen(8080);
