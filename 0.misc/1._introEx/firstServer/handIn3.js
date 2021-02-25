3c134e8b9f222543112b610e8c54fea2d3ea8917
JakobBundgaard <jakobbundgaard@hotmail.com>
2021-02-25 08:58:35 +0100

    with Anders solution'

--- 0.misc/1._introEx/firstServer/handIn3.js
+++ 0.misc/1._introEx/firstServer/handIn3.js
@@ -9,15 +9,24 @@ let dateObj = new Date();
 console.log(dateObj)
 
 app.get("/time", (req, res) => {
-    res.send("The time is: " + dateObj.toLocaleTimeString());
-})
+    res.send("The time is: " + dateObj.toLocaleTimeString('dk-DK', { weekday: 'long' }));
+});
 
 app.get("/day", (req, res) => {
     res.send("The day is: " + dateObj.toDateString().slice(0, 3));
-})
+});
 
 app.get("/month", (req, res) => {
     res.send("The month is: " + dateObj.toDateString().slice(3, 7));
-})
+});
+
+
+// outsideof function, because otherwise the array would be inititalized everytime we run  
+const weekdays = ["Sunday", "Monday", "Tuesday", "wednesday", "Thursday", "Friday", "Saturday"]
+
+app.get("/days", (req, res) => {
+    const todaysWeekday = new Date().getDay(); // needs to be inside functionotherwise the day wouldn't change
+    res.send({ day: weekdays[todaysWeekday] });
+});
 
 app.listen(8080);
