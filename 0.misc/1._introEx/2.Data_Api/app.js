c2d3f4b669b0f95d6a6cd51af287e10fe2f3bfe5
JakobBundgaard <jakobbundgaard@hotmail.com>
2021-02-25 12:24:02 +0100

    post

--- 0.misc/1._introEx/2.Data_Api/app.js
+++ 0.misc/1._introEx/2.Data_Api/app.js
@@ -1,3 +1,26 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+app.get("/querystring", (req, res) => {
+    console.log(req.query);
+    res.send({ query: req.query });
+});
+
+app.get("/pathvariable/:message/:title/:author", (req, res) => {
+    res.send({
+        message: req.params.message,
+        title: req.params.title,
+        author: req.params.author
+    });
+})
+
+
+
+app.post("/whatever", (req, res) => {
+    console.log()
+    res.send({ body: req.body });
+})
+
+app.listen(8080);
